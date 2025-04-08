3743871432ae8d34d6a2c6440aebb3e487f43985
gabincharlemagne <92294664+gabincharlemagne@users.noreply.github.com>
2025-04-08 19:06:14 +0200

    Ajout de la configuration de déploiement GitHub Pages et mise à jour des chemins d'image dans le CV

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://gabincharlemagne.github.io",
+  base: "CvGabinCharlemagne",
+  trailingSlash: "always",
   integrations: [tailwind()],
 });
